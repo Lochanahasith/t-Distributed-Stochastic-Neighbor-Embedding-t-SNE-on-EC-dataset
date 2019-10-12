177d0a86130c6b9bd1538ecd10962df0e9001cfd
Lochanahasith <lochanahm@gmail.com>
2019-10-12 12:55:22 +0530

    apply t-sne and visualization

--- tsne_with_ec.py
+++ tsne_with_ec.py
@@ -12,4 +12,18 @@ datawithnan= pd.read_csv('endometrial.csv')
 
 data1 = datawithnan.dropna() # remove missing values
 
+data1.head()
+
+data = data1.iloc[:,1:]    # remove the first row
+data.head()
+
+scaled_data = preprocessing.scale(data.T) # we use transpose because the scale function expects the samples as rows
+
+tsne = TSNE(n_components=2, n_iter=2500, random_state=0) 
+
+tsne_data = tsne.fit_transform(scaled_data)
+
+plt.scatter(tsne_data[:,0], tsne_data[:,1])
+plt.show()
+
 
